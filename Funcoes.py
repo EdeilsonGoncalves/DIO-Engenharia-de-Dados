1526eef15966a90ad1605b6ee0a10f427ffd61d5
Edeilson Gonçalves <edeilsoniasd@outlook.com>
2024-08-22 15:19:21 -0300

    commit inicial

---
+++ Funcoes.py
@@ -0,0 +1,48 @@
+# Funções 
+
+def exibir_mensagem():
+    print('Olá mundo!')
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+    
+def exibir_mensagem_3(nome="Antônio"):
+    print(f"Seja bem vindo {nome}")
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Edeilson")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Arnaldo")
+
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1 
+    
+    return antecessor, sucessor
+
+def func_3():
+    print("Olá mundo!") 
+    
+print(calcular_total([10, 20, 34])) #64
+print(retorna_antecessor_e_sucessor(10)) # (9, 11)
+print(func_3()) # None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
